c15ba4ecff9211633d569e1fb7c6bcb262250197
Victor Kiprono <victorkipronokirui2@gmail.com>
2025-07-27 22:09:54 +0300

    changed currency

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -64,7 +64,7 @@ export default function Cart() {
                       {item.name}
                     </h3>
                     <p className="text-blue-600 text-sm">
-                      KSH{item.price.toLocaleString()}
+                      AED {item.price.toLocaleString()}
                     </p>
                     
                     {/* Quantity Controls */}
